9fd0c24ef4d6b93fd50f57f253e56c0d43c598e7
matus10 <matus10seznam.cz>
2026-03-13 18:52:27 +0100

    Úprava adresy u modelu klienta, změněna obecná adresa na město, psč, ulice + číslo popisné

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,9 @@ class Client(models.Model):
     personal_id = models.CharField(max_length=10, unique=True)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
-    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
