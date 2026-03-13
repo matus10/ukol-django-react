c9054a7dd0b8dbf0b3cd2f54dddaf982bd3dc118
matus10 <matus10seznam.cz>
2026-03-13 19:08:04 +0100

    Uprava metody destroy() v client_views.py pro ošetření ProcetedError při pokusu o smazání klienta s přiděleným účtem.

--- backend/api/views/client_views.py
+++ backend/api/views/client_views.py
@@ -1,7 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from ..models import Client
 from ..serializers import ClientSerializer
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        try:
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except ProtectedError:
+            return Response({"detail": "Klienta nelze smazat, protože má přidělený účet."}, status=status.HTTP_400_BAD_REQUEST)
